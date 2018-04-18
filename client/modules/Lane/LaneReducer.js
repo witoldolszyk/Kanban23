f11b7576366f412d5b73db734878fe4819bfd79c
witoldolszyk <30417355+witoldolszyk@users.noreply.github.com>
2018-04-18 15:10:32 +0200

    Update LaneReducer.js

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,8 +1,8 @@
-import { CREATE_LANE, CREATE_LANES, UPDATE_LANE, DELETE_LANE, EDIT_LANE } from './LaneActions';
-import { DELETE_NOTE, CREATE_NOTE } from '../Note/NoteActions';
+import { CREATE_LANE, CREATE_LANES, UPDATE_LANE, DELETE_LANE, EDIT_LANE, MOVE_BETWEEN_LANES, REMOVE_FROM_LANE, PUSH_TO_LANE } from './LaneActions';
+import { DELETE_NOTE, CREATE_NOTE, MOVE_WITHIN_LANE} from '../Note/NoteActions';
 
 import omit from 'lodash/omit';
-//initial state
+
 const initialState = {};
 
 export default function lanes(state = initialState, action) {
@@ -12,29 +12,71 @@ export default function lanes(state = initialState, action) {
      return { ...state, [action.lane.id]: action.lane };
 
    case EDIT_LANE: {
-     const lane = { ...state[action.laneId], editing: true };
-     return { ...state, [action.laneId]: lane };
+     const lane = { ...state[action.id], editing: true };
+     return { ...state, [action.id]: lane };
    }
 
    case CREATE_LANES:
      return { ...action.lanes };
-
    case DELETE_NOTE: {
      const newLane = { ...state[action.laneId] };
      newLane.notes = newLane.notes.filter(noteId => noteId !== action.noteId);
 
      return { ...state, [action.laneId]: newLane };
    }
+
    case CREATE_NOTE: {
      const newLane = { ...state[action.laneId] };
      newLane.notes = newLane.notes.concat(action.note.id);
 
      return { ...state, [action.laneId]: newLane };
    }
+
    case DELETE_LANE: {
      return omit(state, action.laneId);
    }
+
+  case MOVE_WITHIN_LANE: {
+    const newLane = {...state[action.laneId] };
+    newLane.notes = moveNotes(newLane.notes, action.sourceId, action.targetId);
+
+    return { ...state, [action.laneId]: newLane};
+   }
+
+  case MOVE_BETWEEN_LANES: {
+    const targetLane = { ...state[action.targetLaneId] };
+    targetLane.notes = [...targetLane.notes, action.noteId];
+
+    const sourceLane = { ...state[action.sourceLaneId] };
+    sourceLane.notes = sourceLane.notes.filter(noteId => noteId !== action.noteId);
+
+    return { ...state, [action.targetLaneId]: targetLane, [action.sourceLaneId]: sourceLane};
+   }
+
+  case REMOVE_FROM_LANE: {
+    const sourceLane = { ...state[action.sourceLaneId] };
+    sourceLane.notes = sourceLane.notes.filter(noteId => noteId !== action.noteId);
+
+    return { ...state, [action.sourceLaneId]: sourceLane};
+   }
+
+  case PUSH_TO_LANE: {
+    const targetLane = { ...state[action.targetLaneId] };
+    targetLane.notes = [...targetLane.notes, action.noteId];
+
+    return { ...state, [action.targetLaneId]: targetLane };
+   }
+
    default:
      return state;
  }
+}
+
+function moveNotes(array, sourceNoteId, targetNoteId) {
+  const sourceIndex = array.indexOf(sourceNoteId);
+  const targetIndex = array.indexOf(targetNoteId);
+  const arrayCopy = [...array];
+
+  arrayCopy.splice(targetIndex, 0, arrayCopy.splice(sourceIndex, 1)[0])
+  return arrayCopy;
 }
