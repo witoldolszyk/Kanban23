9ecdd7ab2563aa1c5fec54e6a98a09c3c95541a5
witoldolszyk <30417355+witoldolszyk@users.noreply.github.com>
2018-04-18 15:07:40 +0200

    Update Notes.js

--- client/modules/Note/Notes.js
+++ client/modules/Note/Notes.js
@@ -1,30 +1,35 @@
 import React, { PropTypes } from 'react';
 import Note from './Note';
-import styles from './Notes.css';
+
 import Edit from '../../components/Edit';
 
-
-const Notes = ({ notes, laneId, editNote, updateNote, deleteNoteRequest, updateNoteRequest }) => {
-  return (<ul className={styles.notes}>{notes.map((note) =>
-    <Note
-      id={note.id}
-      key={note.id}
-      editing={note.editing}
-    >
-      <Edit
-        editing={note.editing}
-        value={note.task}
-        onValueClick={() => editNote(note.id)}
-        onUpdate={(task) => updateNote({
-          ...note,
-          task,
-          editing: false,
-      }
+const Notes = ({ notes, laneId, editNote, updateNote, deleteNote, moveWithinLane}) => {
+  return (
+  <ul className="notes">
+    {notes.map((note, id) =>
+      <Note
+        id={note.id}
+        key={note.id}
+        moveWithinLane={moveWithinLane}
+        laneId={laneId}
+        _id={note._id}
+      >
+        <Edit
+          editing={note.editing}
+          value={note.task}
+          onValueClick={() => editNote(note.id)}
+          onUpdate={(task) => updateNote({
+              ...note,
+              task,
+              editing: false,
+            }
+          )}
+          onDelete={() => deleteNote(note.id, laneId)}
+        />
+      </Note>
     )}
-        onDelete={() => deleteNoteRequest(note.id, laneId)}
-      />
-    </Note>
-  )}</ul>);
+  </ul>
+  );
 };
 
 Notes.propTypes = {
@@ -33,8 +38,7 @@ Notes.propTypes = {
   laneId: PropTypes.string,
   editNote: PropTypes.func,
   notes: PropTypes.array,
-  deleteNoteRequest: PropTypes.func,
-  updateNoteRequest: PropTypes.func,
+  moveWithinLane: PropTypes.func,
 };
 
 export default Notes;
