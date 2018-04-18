3f75183ebeffcc895c7b4fb7410c1a2e7577f0e4
witoldolszyk <30417355+witoldolszyk@users.noreply.github.com>
2018-04-18 15:16:04 +0200

    Update LaneContainer.js

--- client/modules/Lane/LaneContainer.js
+++ client/modules/Lane/LaneContainer.js
@@ -1,22 +1,48 @@
 import { connect } from 'react-redux';
 import Lane from './Lane';
 import * as laneActions from './LaneActions';
-import { createLaneRequest, fetchLanes, deleteLaneRequest, updateLaneRequest } from '../Lane/LaneActions';
-import { deleteLane, updateLane, editLane } from './LaneActions';
-import { createNoteRequest } from '../Note/NoteActions';
+import { compose } from 'redux';
+import { DropTarget } from 'react-dnd';
+import ItemTypes from '../Kanban/itemTypes';
+
+import { updateLaneRequest, deleteLaneRequest, moveBetweenLanes, removeFromLane, pushToLane, changeLanesRequest } from "./LaneActions";
+import { createNote, createNoteRequest } from '../Note/NoteActions';
 
 
-const mapStateToProps = (state, ownProps) => ({
-  laneNotes: ownProps.lane.notes.map(noteId => state.notes[noteId])
-});
+const mapStateToProps = (state, ownProps) => {
+  return {
+    laneNotes: ownProps.lane.notes.map(noteId => {
+      return { ...state.notes[noteId] }
+    })
+  };
+};
 
 const mapDispatchToProps = {
-  editLane,
-  deleteLane: deleteLaneRequest,
+  ...laneActions,
   updateLane: updateLaneRequest,
+  deleteLane: deleteLaneRequest,
   addNote: createNoteRequest,
+  moveBetweenLanes,
+  removeFromLane,
+  changeLanesRequest,
+  pushToLane,
 };
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+
+const noteTarget = {
+  drop(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+    const { id: noteId, laneId: sourceLaneId } = sourceProps;
+    if (targetProps.lane.id !== sourceLaneId) {
+      targetProps.changeLanesRequest(sourceLaneId, targetProps.lane.id, noteId);
+    }
+
+  },
+}
+
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  DropTarget(ItemTypes.NOTE, noteTarget, (dragConnect) => ({
+    connectDropTarget: dragConnect.dropTarget()
+  }))
 )(Lane);
